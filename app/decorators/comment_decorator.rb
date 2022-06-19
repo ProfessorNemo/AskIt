5449b4c8357cddf908d0ecb687b49734434b8646
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-06-19 15:47:54 +0300

    lesson_15 v.2

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -2,15 +2,6 @@
 
 class CommentDecorator < ApplicationDecorator
   delegate_all
+  # задекорировать юзера для комментария
   decorates_association :user
-
-  # commentable - либо ответ, либо вопрос
-  def for?(commentable)
-    # Если commentable задекорировал, то нужно вытащить конкретный образец класса,
-    # потому что draper наворачивает аттрибуты на задекор-й объект.
-    commentable = commentable.object if commentable.decorated?
-    # self указывает на конкретный комментарий и когда говорим "self.commentable",
-    # мы понимаем, для чего комментарий был оставлен (вопроса или ответа)
-    commentable == self.commentable
-  end
 end
